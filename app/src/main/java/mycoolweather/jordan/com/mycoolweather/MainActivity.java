c449f565d06e8a702be8ee5a887759bab001201a
JORDAN23G <yaoyugai@163.com>
2017-11-29 18:27:44 +0800

    更新功能代码

--- app/src/main/java/mycoolweather/jordan/com/mycoolweather/MainActivity.java
+++ app/src/main/java/mycoolweather/jordan/com/mycoolweather/MainActivity.java
@@ -1,13 +1,30 @@
 package mycoolweather.jordan.com.mycoolweather;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.et_content)
+    EditText etContent;
+    @BindView(R.id.btn_click)
+    Button btnClick;
+
+//    @OnClick(R.id.btn_click)
+//    public void showMsg() {
+//        Toast.makeText(this, "btn_click", Toast.LENGTH_SHORT).show();
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //绑定activity
+        ButterKnife.bind(this);
     }
 }
--- app/src/main/java/mycoolweather/jordan/com/mycoolweather/TestGit.java
+++ app/src/main/java/mycoolweather/jordan/com/mycoolweather/TestGit.java
@@ -1,9 +1,39 @@
 package mycoolweather.jordan.com.mycoolweather;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ToggleButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
  * Created by JORDAN23G on 2017/11/28.
  */
 
-public class TestGit {
-    //测试是否添加Github成功
+public class TestGit extends AppCompatActivity {
+
+
+    @BindView(R.id.et_receipt_name)
+    EditText etReceiptName;
+    @BindView(R.id.et_receipt_tel)
+    EditText etReceiptTel;
+    @BindView(R.id.et_receipt_address)
+    EditText etReceiptAddress;
+    @BindView(R.id.btn_setting_msg)
+    ToggleButton btnSettingMsg;
+    @BindView(R.id.ll_commit)
+    LinearLayout llCommit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_address);
+        //绑定activity
+        ButterKnife.bind(this);
+    }
+
+
 }
